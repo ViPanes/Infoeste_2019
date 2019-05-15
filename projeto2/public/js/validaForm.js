e499c4a9401aeffea5cd0831fc0d67bd6df73c26
unknown <vipanes@gmail.com>
2019-05-15 12:08:15 -0300

    Dia 3 - 15/05/19

--- projeto2/public/js/validaForm.js
+++ projeto2/public/js/validaForm.js
@@ -1,6 +1,65 @@
-function valida () {
-    let text = document.getElementById("nome").value;
+function validar() {
+    let codigo = $('#codigo').val();
+    let nome = $('#nome').val();
+    let curso = $('#cursos').val();
 
-if(text=="")
-    alert("Campo obrigatório",null);
+    if(codigo == '' || codigo <= 0)
+    {
+        $('#codigo').addClass('erro');
+        return;
+    }
+
+    $('#codigo').removeClass('erro');   
+
+    if(nome == '' || nome.length > 20)
+    {
+        $('#nome').addClass('erro');
+        return;
+    }
+    
+    $('#nome').removeClass('erro');
+    
+
+    if(curso == '')
+    {
+        $('#cursos').addClass('erro');
+        return;
+    }
+
+    $('#cursos').removeClass('erro');
+    
+    addTable(codigo, nome, curso);
+    limparCampos ();
+}
+
+function limparCampos (){
+    $('#codigo').val('');
+    $('#nome').val('');
+    $('#cursos').val('');
+    $('#email').val('');
+    $('#mensagem').val('');
+}
+
+function addTable (c, n, cu)
+{
+    $('tbody').append(
+        `<tr id='${c}#${n}#${cu}'>
+            <td>${c}</td>
+            <td>${n}</td>
+            <td onclick="removeTabela(this)">X</td>
+            <td onclick="alt(this)">${cu}</td>
+        </tr>`
+    );
+}
+
+function removeTabela(td) {
+    $(td.closest('tr')).remove();
+}
+
+function alt(td) {
+    let data = td.closest('tr').id.split('#');
+
+    $('#codigo').val(data[0]);
+    $('#nome').val(data[1]);
+    $('#cursos').val(data[2]);
 }
